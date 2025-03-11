06977fb2e27361febdc5a65a99ca0527967f86cf
Michael Fransman <30863830+mikeyfe6@users.noreply.github.com>
2025-03-11 13:32:50 +0100

    botfield and some fixes...

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,132 +1,134 @@
-import React from 'react';
+import React from "react";
 
-import { graphql, useStaticQuery, Link } from 'gatsby';
-import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+import { graphql, useStaticQuery, Link } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
-import { useSiteMetadata } from '../hooks/use-site-metadata';
+import { useSiteMetadata } from "../hooks/use-site-metadata";
 
-import * as footerStyles from '../styles/modules/footer.module.scss';
+import * as footerStyles from "../styles/modules/footer.module.scss";
 
 const Footer: React.FC = () => {
-	const data = useStaticQuery(graphql`
-		query {
-			pzLogo: file(relativePath: { eq: "logo/pz-logo.jpg" }) {
-				childImageSharp {
-					gatsbyImageData
-				}
-			}
-		}
-	`);
+    const data = useStaticQuery(graphql`
+        query {
+            pzLogo: file(relativePath: { eq: "logo/pz-logo.jpg" }) {
+                childImageSharp {
+                    gatsbyImageData
+                }
+            }
+        }
+    `);
 
-	const pzLogo = getImage(data.pzLogo.childImageSharp.gatsbyImageData);
+    const pzLogo = getImage(data.pzLogo.childImageSharp.gatsbyImageData);
 
-	const {
-		companyName,
-		address,
-		postalCode,
-		city,
-		phone,
-		phoneRaw,
-		email,
-		kvk,
-		btw,
-		bank,
-	} = useSiteMetadata();
+    const {
+        companyName,
+        address,
+        postalCode,
+        city,
+        phone,
+        phoneRaw,
+        email,
+        kvk,
+        btw,
+        bank,
+    } = useSiteMetadata();
 
-	const currentYear = new Date().getFullYear();
+    const currentYear = new Date().getFullYear();
 
-	return (
-		<footer className={footerStyles.footer}>
-			<div className={footerStyles.footerTopBar}>
-				<div className={footerStyles.footerTopWrapper}>
-					<div className={footerStyles.list}>
-						<h6>{companyName}</h6>
-						<ul>
-							<li>{address}</li>
-							<li>
-								{postalCode} {city}
-							</li>
-							{/* <li>
+    return (
+        <footer className={footerStyles.footer}>
+            <div className={footerStyles.footerTopBar}>
+                <div className={footerStyles.footerTopWrapper}>
+                    <div className={footerStyles.list}>
+                        <h6>{companyName}</h6>
+                        <ul>
+                            <li>{address}</li>
+                            <li>
+                                {postalCode} {city}
+                            </li>
+                            {/* <li>
 								<a href={`tel:${phoneRaw}`}>
 									<i className='fa-solid fa-phone fa-lg' />
 									<span>{phone}</span>
 								</a>
 							</li> */}
-							<li>
-								<a href={`mailto:${email}`}>
-									<i className='fa-solid fa-envelope fa-lg' />
-									<span>{email}</span>
-								</a>
-							</li>
-							<li>KVK: {kvk}</li>
-							<li>BTW: {btw}</li>
-							<li>IBAN: {bank}</li>
-						</ul>
-					</div>
+                            <li>
+                                <a href={`mailto:${email}`}>
+                                    <i className="fa-solid fa-envelope fa-lg" />
+                                    <span>{email}</span>
+                                </a>
+                            </li>
+                            <li>KVK: {kvk}</li>
+                            <li>BTW: {btw}</li>
+                            <li>IBAN: {bank}</li>
+                        </ul>
+                    </div>
 
-					<div className={footerStyles.general}>
-						<h6>Algemeen</h6>
-						<ul>
-							<li>
-								<Link to='/over-ons/'>Over Prio Zorg</Link>
-							</li>
-							<li>
-								<Link to='/contact/'>Contact</Link>
-							</li>
-							<li></li>
-						</ul>
-					</div>
+                    <div className={footerStyles.general}>
+                        <h6>Algemeen</h6>
+                        <ul>
+                            <li>
+                                <Link to="/over-ons/">Over Prio Zorg</Link>
+                            </li>
+                            <li>
+                                <Link to="/contact/">Contact</Link>
+                            </li>
+                            <li></li>
+                        </ul>
+                    </div>
 
-					<div className={footerStyles.socials} id='footer-socials'>
-						<h6>Volg Ons</h6>
-						<ul>
-							{/* <li>
+                    <div className={footerStyles.socials} id="footer-socials">
+                        <h6>Volg Ons</h6>
+                        <ul>
+                            {/* <li>
 								<a href='#!'>
 									<i className='fa-brands fa-facebook' />
 								</a>
 							</li> */}
-							{/* <li>
+                            {/* <li>
 								<a href='#!'>
 									<i className='fa-brands fa-instagram' />{' '}
 								</a>
 							</li> */}
-							<li>
-								<a
-									href='https://www.linkedin.com/in/prio-zorg-a4b783285/'
-									rel='noopener noreferrer'
-									target='_blank'>
-									<i className='fa-brands fa-linkedin' />{' '}
-								</a>
-							</li>
-						</ul>
-					</div>
-					<div className={footerStyles.logo} id='footer-logo'>
-						{pzLogo && (
-							<Link to='/'>
-								<GatsbyImage image={pzLogo} alt='PZ Logo' />
-							</Link>
-						)}
-					</div>
-				</div>
-			</div>
-			<div className={footerStyles.footerBottomBar} id='footer-bottombar'>
-				<div className={footerStyles.footerBottomWrapper}>
-					<div>
-						<b>©</b> {currentYear} · {companyName}
-					</div>
-					<div>
-						Powered by{' '}
-						<a
-							href='https://menefex.nl/'
-							rel='noopener noreferrer'
-							target='_blank'>
-							Menefex
-						</a>
-					</div>
-				</div>
-			</div>
-		</footer>
-	);
+                            <li>
+                                <a
+                                    href="https://www.linkedin.com/in/prio-zorg-a4b783285/"
+                                    rel="noopener noreferrer"
+                                    target="_blank"
+                                >
+                                    <i className="fa-brands fa-linkedin" />{" "}
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className={footerStyles.logo} id="footer-logo">
+                        {pzLogo && (
+                            <Link to="/">
+                                <GatsbyImage image={pzLogo} alt="PZ Logo" />
+                            </Link>
+                        )}
+                    </div>
+                </div>
+            </div>
+            <div className={footerStyles.footerBottomBar} id="footer-bottombar">
+                <div className={footerStyles.footerBottomWrapper}>
+                    <div>
+                        <b>©</b> {currentYear} · {companyName}
+                    </div>
+                    <div>
+                        Webcrafted by{" "}
+                        <a
+                            href="https://menefex.nl/"
+                            rel="noopener noreferrer"
+                            target="_blank"
+                        >
+                            Menefex
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,88 +1,91 @@
-import React from 'react';
+import React from "react";
 
-import { useSiteMetadata } from '../hooks/use-site-metadata';
+import { useSiteMetadata } from "../hooks/use-site-metadata";
 
-import * as formStyles from '../styles/modules/form.module.scss';
+import * as formStyles from "../styles/modules/form.module.scss";
 
 const Form: React.FC = () => {
-	const {
-		companyName,
-		address,
-		postalCode,
-		city,
-		// phone,
-		// phoneRaw,
-		email,
-		kvk,
-		btw,
-		bank,
-	} = useSiteMetadata();
+    const {
+        companyName,
+        address,
+        postalCode,
+        city,
+        // phone,
+        // phoneRaw,
+        email,
+        kvk,
+        btw,
+        bank,
+    } = useSiteMetadata();
 
-	return (
-		<div className={formStyles.form}>
-			<div>
-				<ul>
-					<li>{companyName}</li>
-					<li>{address}</li>
-					<li>
-						{postalCode} {city}
-					</li>
-					{/* <li>
+    return (
+        <div className={formStyles.form}>
+            <div>
+                <ul>
+                    <li>{companyName}</li>
+                    <li>{address}</li>
+                    <li>
+                        {postalCode} {city}
+                    </li>
+                    {/* <li>
 						<span>Telefoon:</span> <a href={`tel:${phoneRaw}`}>{phone}</a>
 					</li> */}
-					<li>
-						<span>E-mailadres:</span> <a href={`mailto:${email}`}>{email}</a>
-					</li>
-					<li>
-						<span>KVK:</span> {kvk}
-					</li>
-					<li>
-						<span>BTW:</span> {btw}
-					</li>
-					<li>
-						<span>IBAN:</span> {bank}
-					</li>
-				</ul>
-			</div>
-			<form
-				name='priozorg-netlifyform'
-				method='POST'
-				data-netlify='true'
-				data-netlify-honeypot='bot-field'
-				action='/success/'>
-				<input type='hidden' name='form-name' value='priozorg-netlifyform' />
+                    <li>
+                        <span>E-mailadres:</span>{" "}
+                        <a href={`mailto:${email}`}>{email}</a>
+                    </li>
+                    <li>
+                        <span>KVK:</span> {kvk}
+                    </li>
+                    <li>
+                        <span>BTW:</span> {btw}
+                    </li>
+                    <li>
+                        <span>IBAN:</span> {bank}
+                    </li>
+                </ul>
+            </div>
+            <form
+                name="pz-form"
+                method="POST"
+                data-netlify="true"
+                data-netlify-honeypot="bot-field"
+                action="/success/"
+            >
+                <input type="hidden" name="bot-field" />
+                <input type="hidden" name="form-name" value="pz-form" />
 
-				<h5>Vul hieronder je gegevens in</h5>
-				<div>
-					<label htmlFor='formFirstName'>Voornaam</label>
-					<input type='text' name='firstName' id='formFirstName' />
-				</div>
-				<div>
-					<label htmlFor='formLastName'>Achternaam</label>
-					<input type='text' name='lastName' id='formLastName' />
-				</div>
-				<div>
-					<label htmlFor='formCompany'>Bedrijfsnaam</label>
-					<input type='text' name='company' id='formCompany' />
-				</div>
-				<div>
-					<label htmlFor='formPhoneNr'>Telefoonnummer</label>
-					<input type='tel' name='phone' id='formPhoneNr' />
-				</div>
-				<div>
-					<label htmlFor='formEmail'>E-mailadres</label>
-					<input type='email' name='email' id='formEmail' />
-				</div>
-				<div>
-					<label htmlFor='formMessage'>Bericht</label>
-					<textarea name='message' id='formMessage' rows={7} />
-				</div>
-				<div>
-					<button type='submit'>Versturen</button>
-				</div>
-			</form>
-		</div>
-	);
+                <h5>Vul hieronder je gegevens in</h5>
+                <div>
+                    <label htmlFor="formFirstName">Voornaam</label>
+                    <input type="text" name="firstName" id="formFirstName" />
+                </div>
+                <div>
+                    <label htmlFor="formLastName">Achternaam</label>
+                    <input type="text" name="lastName" id="formLastName" />
+                </div>
+                <div>
+                    <label htmlFor="formCompany">Bedrijfsnaam</label>
+                    <input type="text" name="company" id="formCompany" />
+                </div>
+                <div>
+                    <label htmlFor="formPhoneNr">Telefoonnummer</label>
+                    <input type="tel" name="phone" id="formPhoneNr" />
+                </div>
+                <div>
+                    <label htmlFor="formEmail">E-mailadres</label>
+                    <input type="email" name="email" id="formEmail" />
+                </div>
+                <div>
+                    <label htmlFor="formMessage">Bericht</label>
+                    <textarea name="message" id="formMessage" rows={7} />
+                </div>
+                <div>
+                    <button type="submit">Versturen</button>
+                </div>
+            </form>
+        </div>
+    );
 };
 
 export default Form;
